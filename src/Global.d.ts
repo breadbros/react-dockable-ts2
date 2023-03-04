c3d5a5c8a4089545ba8037e3eaf2f4f032fd6811
Ben McGraw <ben.mcgraw@gmail.com>
2023-03-03 19:20:48 -0800

    Add new typings

--- src/Global.d.ts
+++ src/Global.d.ts
@@ -7,3 +7,32 @@ declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
 }
+
+declare type PanelState = {
+  size?: number;
+  minSize?: number;
+  resize?: 'fixed' | 'dynamic' | 'stretch';
+  windows: WindowState[];
+};
+
+declare type WindowState = {
+  selected: number | 0;
+  widgets: string[];
+};
+
+declare type DockableProps = {
+  initialState: PanelState[];
+  hidden?: {
+    [key: string]: boolean;
+  };
+  onUpdate: (arg: PanelState[]) => void;
+  onActive?: (id: string) => void;
+  themeClass?: string;
+  theme?: any;
+  spacing?: number;
+  hideMenus?: boolean;
+  hideTabs?: boolean;
+  active?: string;
+  tabHeight?: number;
+  children?: JSX.Element | JSX.Element[];
+};
