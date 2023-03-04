8d4d3863dc97460ace2f0d3990a3e0be059ab024
Ben McGraw <ben.mcgraw@gmail.com>
2023-03-03 20:03:37 -0800

    Export types

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Dockable from './dockable/Dockable';
 import { Widget } from './dockable/Widget';
+import { PanelState, WindowState, DockableProps } from 'types';
 
 export default Dockable;
-export { Widget };
+export { Widget, PanelState, WindowState, DockableProps };
